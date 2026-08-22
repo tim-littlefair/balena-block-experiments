825838e7718cc72329f966c029e0148c2e43d5ca
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2026-08-22 16:09:41 +0800

    Modernized xserver-server-block now working
    Brings up matchbox-window-manager and xterm
    USB mouse+pointer working

--- modernized/xserver-server-block/src/entry.sh
+++ modernized/xserver-server-block/src/entry.sh
@@ -3,16 +3,6 @@
 # kill previous x11 sockets that have persisted
 rm -r /tmp/.X11-unix 2>/dev/null
 
-uuid7=$(echo $BALENA_DEVICE_UUID | cut -c 1-7)
-#hostname uuid7
-cat /etc/hostname 
-cat /etc/hosts
-
-
-echo Running base image entry.sh
-/bin/bash /opt/xserver/balenalib-xserver-entry.sh echo "Running balena base image entrypoint..."
-echo Base image entry.sh has run
-
 echo "Setting initial display to FORCE_DISPLAY - $FORCE_DISPLAY"
 
 # destroy any leftover X11 lockfile. credit to @danclimasevschi
@@ -38,10 +28,11 @@ then
 	fi
 fi
 
-DISPLAY=$FORCE_DISPLAY xauth -n
+export DISPLAY=$FORCE_DISPLAY
 if [ "$CURSOR" = true ];
 then
-    exec startx $FORCE_DISPLAY
+    exec startx
 else
-    exec startx $FORCE_DISPLAY -nocursor
+    exec startx -nocursor
 fi
+
